4b1f75561ba040c84315224c4e23ed78e37985a5
miaojundev <1079102985@qq.com>
2019-12-26 17:36:29 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/example/zzz/weather/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.example.zzz.weather.util;
+
+/**
+ * Created by zzz on 2019/12/26.
+ */
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
