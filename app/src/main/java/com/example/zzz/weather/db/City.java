2e81ae5aed4b0af9526bd32ddf1706ee48947d97
miaojundev <1079102985@qq.com>
2019-12-26 15:42:27 +0800

    创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/zzz/weather/db/City.java
@@ -0,0 +1,52 @@
+package com.example.zzz.weather.db;
+
+import org.litepal.LitePal;
+
+/**
+ * Created by zzz on 2019/12/26.
+ */
+
+
+public class City extends LitePal {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
---
+++ app/src/main/java/com/example/zzz/weather/db/Province.java
@@ -0,0 +1,39 @@
+package com.example.zzz.weather.db;
+
+import org.litepal.LitePal;
+
+/**
+ * Created by zzz on 2019/12/26.
+ */
+
+public class Province extends LitePal {
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
